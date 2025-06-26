998b0ec8798eb0ebb77cd7a7e04bd6ed4151a5db
c25kenneth <c25kenneth@gmail.com>
2025-06-26 15:56:36 -0700

    Revert cors config

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,9 @@ def create_app():
     app = Flask(__name__)
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-    # CORS(app)
-    CORS(app, resources={r"/*": {"origins": "https://gentle-dune-04f5fbc1e.6.azurestaticapps.net"}}, supports_credentials=True)
-    
+    CORS(app)
+    # CORS(app, resources={r"/*": {"origins": "https://gentle-dune-04f5fbc1e.6.azurestaticapps.net"}}, supports_credentials=True)
+
     app.register_blueprint(chat_bp)
     app.register_blueprint(user_bp)
 
